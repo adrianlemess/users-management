c9fed98463564ef09aa88e1b05f4d3d27c423f07
Adrian Lemes <adrianlemess@gmail.com>
2024-03-01 20:34:06 +0100

    chore(vite.config.ts): update api proxy url
    Updated proxy url with reqres.in API

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://reqres.in/api",
         secure: false,
         rewrite: path => path.replace(/^\/api/, ""),
       },
