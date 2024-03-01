4b82da68246ce122114beb3ceb300fd756516578
Adrian Lemes <adrianlemess@gmail.com>
2024-03-02 00:35:37 +0100

    chore: deps and configs update
    - Update all package.json dependencies
    - Add prettier ignore
    - Update commit lint
    - Remove warning about cjs
    - Add CI to validate the code

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
-import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), react()],
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 200,
+
+    rollupOptions: {
+      output: {
+        // This will handle the code splitting for the node_modules separately
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
