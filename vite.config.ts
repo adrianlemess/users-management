773311b8ca5c26db187ce932b9f6007b9dfb990a
Adrian Lemes <adrianlemess@gmail.com>
2024-03-03 00:04:25 +0100

    fix: deploy issue with frame-emotion dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,46 +2,35 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 200,
-
-    rollupOptions: {
-      output: {
-        // This will handle the code splitting for the node_modules separately
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+export default ({ mode }: { mode: string }) => {
+  return defineConfig({
+    plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 200,
+      minify: mode === "production",
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+            return;
           }
+          warn(warning);
         },
       },
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        warn(warning);
+    },
+    resolve: {
+      alias: {
+        // for TypeScript path alias import like : @/x/y/z
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
-  resolve: {
-    alias: {
-      // for TypeScript path alias import like : @/x/y/z
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://reqres.in/api",
-        secure: false,
-        rewrite: path => path.replace(/^\/api/, ""),
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://reqres.in/api",
+          secure: false,
+          rewrite: path => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-});
+  });
+};
