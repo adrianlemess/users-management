95559b776a54ba0c7ffb34493bb21fb28b3fd4d5
Adrian Lemes <adrianlemess@gmail.com>
2024-03-03 19:34:35 +0100

    test: fix specs

--- src/pages/Dashboard/Dashboard.spec.tsx
+++ src/pages/Dashboard/Dashboard.spec.tsx
@@ -1,16 +1,17 @@
 import { render, screen } from "@testing-library/react";
 
 import Dashboard from "./Dashboard";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Dashboard", () => {
   it("should renders a msg", () => {
     // arrange
-    render(<Dashboard msg="Hello React!" />);
+    render(<Dashboard />, { wrapper: BrowserRouter });
 
     // act
     const title = screen.getByTestId("title");
 
     // assert
-    expect(title).toHaveTextContent(/Hello React!/i);
+    expect(title).toHaveTextContent(/Welcome/i);
   });
 });
