1a27590938dcb567ff10e2b8f1caf4ccbb9233c7
Adrian Lemes <adrianlemess@gmail.com>
2024-03-06 02:02:56 +0100

    test: smoke test
    - Add smoke test scenario
    - Add command to login
    - Add dashboard selectors

---
+++ cypress/e2e/selectors/dashboard.ts
@@ -0,0 +1,6 @@
+export const DASHBOARD_SELECTORS = {
+  skeleton: '[data-testid="card-skeleton"]',
+  lightToggleTheme: '[aria-label="toggle light mode"]',
+  darkToggleTheme: '[aria-label="toggle dark mode"]',
+  submitButton: 'button[type="submit"]',
+};
